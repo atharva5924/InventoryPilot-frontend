9755cef9816b57afa920d637edaf55f68a56b4e1
Atharva Nile <atharvanile2005@gmail.com>
2025-07-25 00:13:39 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setError("");
     try {
       const res = await axios.post(
-        "http://localhost:5000/users/login",
+        "https://inventorypilot-backend-ib2e.onrender.com/users/login",
         formData,
         {
           withCredentials: true,
